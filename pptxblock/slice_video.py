e0434aa1d819fe56f8660f1603d41bb16b87f9d5
nguyen doan tung <nguyendoantung@hotmail.com>
2017-11-17 02:30:02 +1300

    8

--- pptxblock/slice_video.py
+++ pptxblock/slice_video.py
@@ -18,15 +18,15 @@ class SliceVideo(threading.Thread):
 
     def run(self):
         # print "Starting " + self.name
-        new_path = '/vids/new_days.txt'
-        new_days = open(new_path, 'w')
+        # new_path = '/vids/new_days.txt'
+        # new_days = open(new_path, 'w')
 
-        title = 'Video Test writing\n'
-        new_days.write(title)
-        print(title)
-        new_days.close()
-        # download_cmd = ('youtube-dl {1} -o /vids/{0}/{0}.mp4').format(self.video_id, self.video_url)
-        # os.system(download_cmd)
+        # title = 'Video Test writing\n'
+        # new_days.write(title)
+        # print(title)
+        # new_days.close()
+        download_cmd = ('youtube-dl {1} -o /vids/{0}/{0}.mp4').format(self.video_id, self.video_url)
+        #os.system(download_cmd)
         
         # slice_cmd = ('scenedetect -i /vids/{0}/{0}.mp4 -co /vids/{0}/{0}.csv -d content -si -df 4').format(self.video_id)
         # os.system(slice_cmd)
@@ -40,9 +40,3 @@ class SliceVideo(threading.Thread):
         #     self.thumbs_html += "</h4></div></li>"
         # self.timestamps = timestamps
         # self.thumbs_html += fline
-
-
-# def print_time(thread_name, counter, delay):
-#     "OK docstring"
-#     threadName = ""
-#     return
